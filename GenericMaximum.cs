38fc630dd938d1003c52840a6136e13a90a4a628
“00Amit0” <amit836961@gmail.com>
2022-06-06 00:10:08 +0530

    Checks for max using Generics

---
+++ GenericMaximum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    /// <summary>
+    /// program to check for maximum out of 3 using GENERICS
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class GenericMaximum<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length - 1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is:" + max);
+        }
+    }
+}
